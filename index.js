a648d30ac6ad116e8880b30a033c226469dee3a9
aluzdev <aluzdev@gmail.com>
2024-05-20 15:23:41 -0600

    mongo definition

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 //levantar la aplicación
 //encargado de levantar la conexión con la base de datos
+require("dotenv").config();
 const server = require("./src/server");
+const db = require("./src/lib/db");
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server is listening on port: ${port}`);
-});
+db.connect()
+  .then(() => {
+    console.log("DB Connected");
+
+    server.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("DB Error: ", error);
+  });
---
+++ src/lib/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+
+function connect() {
+  return mongoose.connect();
+}
+
+module.exports = {
+  connect,
+};
